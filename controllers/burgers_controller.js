e8fbe16f78ca6009a1f310a3472c38e6b03a4030
Secure Cap <Twilliams397tw@gmail.com>
2018-09-06 18:42:02 -0500

    controller and router functions

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,14 @@
+var express = require("express");
+
+var router = express.Router();
+
+//burger.js model imported to use database functions
+var burger = require("../models/burger.js");
+
+//all routes and logic within those routes where required
+
+
+
+
+//exports routes for server.js to use
+module.exports = router;
---
+++ models/burger.js
@@ -0,0 +1,7 @@
+//import the orm to create functions that will interact with the database
+var orm = require("../config/orm.js");
+
+
+
+
+

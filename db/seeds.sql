b5519560413691001f16c2fea8d47381af4a5829
Secure Cap <Twilliams397tw@gmail.com>
2018-09-06 17:48:15 -0500

    created server directory and schema files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger-name, devoured) VALUES ('Turkey with cheese', false);
+INSERT INTO burgers (burger-name, devoured) VALUES ('Chicken burger with fried egg', false);
+INSERT INTO burgers (burger-name, devoured) VALUES ('Vegan burger with kale', false);

c9ab2151fd3112e1dec48573b76c889fef1e51e0
Secure Cap <Twilliams397tw@gmail.com>
2018-09-07 18:17:35 -0500

    final commit

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -6,7 +6,53 @@ var router = express.Router();
 var burger = require("../models/burger.js");
 
 //all routes and logic within those routes where required
+router.get("/", function(req, res) {
+    burger.all(function(data) {
+        var hbsObject = {
+            burgers: data
+        };
+        console.log(hbsObject);
+        res.render("index", hbsObject);
+    });
+});
+router.post("/api/burgers", function(req, res) {
+    burger.create([
+        "name", "devoured"
+    ], [
+        req.body.name, req.body.sleepy
+    ], function(result) {
+        //sends back the id of the new burger
+        res.json({ id: result.insertId });
+    });
+});
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
 
+    console.log("condition", condition);
+
+    burger.update({
+        sleepy: req.body.devoured
+    }, condition, function(result) {
+        if (result.changedRows == 0) {
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
+
+router.delete("/api/cats/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(result) {
+        if (result.affectRows == 0) {
+            //if no rows were changed the id must not exists, so 404
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    });
+});
 
 
 
